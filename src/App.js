27f933152ddab93a4e30193a910e0dba701d5abf
Vivek Dogra <vivekdogra02@gmail.com>
2020-02-18 14:01:27 +0530

    Updated routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Homepage from './components/Homepage/Homepage';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Switch>
+        <Route exact path='' component={Homepage} />
+        <Route path= '/hats' component={Homepage} />
+      </Switch>
       <Homepage />
     </div>
   );
