e0ec684d185b9ea025bfae4dd30a1b61fcc35665
Vivek Dogra <vivekdogra02@gmail.com>
2020-02-18 13:30:11 +0530

    Styling the homepage and components

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,40 +1,9 @@
 import React from 'react';
 import './Homepage.scss';
+import Directory from '../directory/directory';
 const Homepage = () => (
     <div className='homepage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>HATS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>JACKETS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>WOMENS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>MENS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>CHILD</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-
-        </div>
+        <Directory />
     </div>
 )
 
