7e2cd58b8a91f83f68506bdfba8e9d40dd399770
Vivek Dogra <vivekdogra02@gmail.com>
2020-02-25 19:41:58 +0530

    adding compression for gzipping

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compress = require('compression');
+
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,6 +12,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(compress());
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
